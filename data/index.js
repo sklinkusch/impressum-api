b200ac862533723db0cbed5c89c7693f22c867d7
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-08-26 20:35:01 +0200

    first data

---
+++ data/index.js
@@ -0,0 +1,79 @@
+const tmg = {
+  title: 'Angaben gemäß § 5 TMG',
+  content: [
+    'Peter R. Stuhlmann',
+    'Martha-Arendsee-Str. 10',
+    '12681 Berlin',
+    'Telefon: +49 (0) 1578 5630944',
+    'E-Mail: info@peter-stuhlmann.de',
+    'Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.',
+  ],
+};
+const inhalte = {
+  title: 'Haftung für Inhalte',
+  content:
+    'Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.',
+};
+const links = {
+  title: 'Haftung für Links',
+  content:
+    'Unser Angebot enthält Links zu externen Webseiten Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.',
+};
+const urheberrecht = {
+  title: 'Urheberrecht',
+  content: [
+    'Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet. Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.',
+    'Quelle: https://www.e-recht24.de',
+  ],
+};
+const datenschutzerklaerung = {
+  title: 'Daten­schutz­erklärung',
+  content:
+    'Diese Datenschutzerklärung klärt Sie über die Art, den Umfang und Zweck der Verarbeitung von personenbezogenen Daten (nachfolgend kurz „Daten“) innerhalb unseres Onlineangebotes und der mit ihm verbundenen Webseiten, Funktionen und Inhalte sowie externen Onlinepräsenzen, wie z.B. unser Social Media Profile auf (nachfolgend gemeinsam bezeichnet als „Onlineangebot“). Im Hinblick auf die verwendeten Begrifflichkeiten, wie z.B. „Verarbeitung“ oder „Verantwortlicher“ verweisen wir auf die Definitionen im Art. 4 der Datenschutzgrundverordnung (DSGVO).',
+};
+const verantwortlicher = {
+  title: 'Verantwortlicher',
+  content: [
+    'Peter R. Stuhlmann',
+    'Martha-Arendsee-Str. 10',
+    '12681 Berlin',
+    'Mobil: +49 1578 5630944',
+    'E-Mail: info@peter-stuhlmann.de',
+  ],
+};
+const datenarten = {
+  title: 'Arten der verarbeiteten Daten',
+  content: [
+    'Bestandsdaten (z.B. Namen, Adressen)',
+    'Kontaktdaten (z.B. E-Mail, Telefonnummer)',
+    'Inhaltsdaten (z.B. Texteingaben, Fotografien, Videos)',
+    'Nutzungsdaten (z.B. besuchte Webseiten, Interesse an Inhalten, Zugriffszeiten)',
+    'Meta-/Kommunikationsdaten (z.B. Geräteinformationen, IP-Adressen)',
+  ],
+};
+const betroffenePersonen = {
+  title: 'Kategorien betroffener Personen',
+  content: `Besucher und Nutzer des Onlineangebotes (Nachfolgend bezeichnen wir die betroffenen Personen zusammenfassend auch als "Nutzer")`,
+};
+exports.data = {
+  title: [
+    tmg.title,
+    inhalte.title,
+    links.title,
+    urheberrecht.title,
+    datenschutzerklaerung.title,
+    verantwortlicher.title,
+    datenarten.title,
+    betroffenePersonen.title,
+  ],
+  content: [
+    tmg.content,
+    inhalte.content,
+    links.content,
+    urheberrecht.content,
+    datenschutzerklaerung.content,
+    verantwortlicher.content,
+    datenarten.content,
+    betroffenePersonen.content,
+  ],
+};
