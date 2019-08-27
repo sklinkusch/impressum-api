0a92e8f5665c5a608f9cfa9219ee2727a3846976
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-08-27 19:42:09 +0200

    outsource tmg

---
+++ data/impressum/tmg.js
@@ -0,0 +1,11 @@
+exports.tmg = {
+  title: 'Angaben gemäß § 5 TMG',
+  content: [
+    'Peter R. Stuhlmann',
+    'Martha-Arendsee-Str. 10',
+    '12681 Berlin',
+    'Telefon: +49 (0) 1578 5630944',
+    'E-Mail: info@peter-stuhlmann.de',
+    'Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.',
+  ],
+};
--- data/index.js
+++ data/index.js
@@ -1,14 +1,5 @@
-const tmg = {
-  title: 'Angaben gemäß § 5 TMG',
-  content: [
-    'Peter R. Stuhlmann',
-    'Martha-Arendsee-Str. 10',
-    '12681 Berlin',
-    'Telefon: +49 (0) 1578 5630944',
-    'E-Mail: info@peter-stuhlmann.de',
-    'Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.',
-  ],
-};
+const tmg_import = require('./impressum/tmg');
+const { tmg } = tmg_import;
 const inhalte = {
   title: 'Haftung für Inhalte',
   content:
