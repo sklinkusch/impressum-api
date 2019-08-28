6967e810ef7083f53f88f43808c28d96f02d8aee
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-08-28 19:25:31 +0200

    put address in new file

---
+++ data/address/index.js
@@ -0,0 +1,7 @@
+exports.address = [
+  'Peter R. Stuhlmann',
+  'Martha-Arendsee-Str. 10',
+  '12681 Berlin',
+  'Mobil: +49 1578 5630944',
+  'E-Mail: info@peter-stuhlmann.de',
+];
--- data/impressum/tmg.js
+++ data/impressum/tmg.js
@@ -1,11 +1,8 @@
+const { address } = require('../address');
+const streit =
+  'Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.';
+const fullArray = address.concat(streit);
 exports.tmg = {
   title: 'Angaben gemäß § 5 TMG',
-  content: [
-    'Peter R. Stuhlmann',
-    'Martha-Arendsee-Str. 10',
-    '12681 Berlin',
-    'Telefon: +49 (0) 1578 5630944',
-    'E-Mail: info@peter-stuhlmann.de',
-    'Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.',
-  ],
+  content: fullArray,
 };
