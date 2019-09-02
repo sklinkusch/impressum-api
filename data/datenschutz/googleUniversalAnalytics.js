a735144ff10fd9298d77e798af66e864c207e7fb
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-09-02 10:47:01 +0200

    trim further white spaces

--- data/datenschutz/googleUniversalAnalytics.js
+++ data/datenschutz/googleUniversalAnalytics.js
@@ -1,5 +1,4 @@
 exports.googleUniversal = {
   title: 'Google Universal Analytics',
-  content: `Wir setzen Google Analytics in der Ausgestaltung als "Universal-Analytics" ein. "Universal Analytics" bezeichnet ein Verfahren von Google Analytics, bei dem die Nutzeranalyse auf Grundlage einer pseudonymen Nutzer-ID erfolgt und damit ein pseudonymes Profil des Nutzers mit Informationen
-  aus der Nutzung verschiedener Geräte erstellt wird (sog. „Cross-Device-Tracking“).`,
+  content: `Wir setzen Google Analytics in der Ausgestaltung als "Universal-Analytics" ein. "Universal Analytics" bezeichnet ein Verfahren von Google Analytics, bei dem die Nutzeranalyse auf Grundlage einer pseudonymen Nutzer-ID erfolgt und damit ein pseudonymes Profil des Nutzers mit Informationen aus der Nutzung verschiedener Geräte erstellt wird (sog. „Cross-Device-Tracking“).`,
 };
