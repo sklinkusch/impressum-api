1cdaba27a0610d971c172291f4eba90e57290a94
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-09-02 10:05:36 +0200

    trim some spaces

--- data/datenschutz/rechteBetroffener.js
+++ data/datenschutz/rechteBetroffener.js
@@ -2,7 +2,7 @@ exports.rechteBetroffener = {
   title: 'Rechte der betroffenen Personen',
   content: [
     'Sie haben das Recht, eine Bestätigung darüber zu verlangen, ob betreffende Daten verarbeitet werden und auf Auskunft über diese Daten sowie auf weitere Informationen und Kopie der Daten entsprechend Art. 15 DSGVO.',
-    'Sie haben entsprechend. Art. 16 DSGVO das Recht, die Vervollständigung    der Sie betreffenden Daten oder die Berichtigung der Sie betreffenden unrichtigen Daten zu verlangen.',
+    'Sie haben entsprechend. Art. 16 DSGVO das Recht, die Vervollständigung der Sie betreffenden Daten oder die Berichtigung der Sie betreffenden unrichtigen Daten zu verlangen.',
     'Sie haben nach Maßgabe des Art. 17 DSGVO das Recht zu verlangen, dass betreffende Daten unverzüglich gelöscht werden, bzw. alternativ nach Maßgabe des Art. 18 DSGVO eine Einschränkung der Verarbeitung der Daten zu verlangen.',
     'Sie haben das Recht zu verlangen, dass die Sie betreffenden Daten, die Sie uns bereitgestellt haben nach Maßgabe des Art. 20 DSGVO zu erhalten und deren Übermittlung an andere Verantwortliche zu fordern.',
     'Sie haben ferner gem. Art. 77 DSGVO das Recht, eine Beschwerde bei der zuständigen Aufsichtsbehörde einzureichen',
--- data/datenschutz/widerspruchsrecht.js
+++ data/datenschutz/widerspruchsrecht.js
@@ -1,5 +1,5 @@
 exports.widerspruchsrecht = {
   title: 'Widerspruchsrecht',
   content:
-    'Sie können der künftigen Verarbeitung der Sie betreffenden Daten nach  Maßgabe des Art. 21 DSGVO jederzeit widersprechen. Der Widerspruch kann insbesondere gegen die Verarbeitung für Zwecke der Direktwerbung erfolgen.',
+    'Sie können der künftigen Verarbeitung der Sie betreffenden Daten nach Maßgabe des Art. 21 DSGVO jederzeit widersprechen. Der Widerspruch kann insbesondere gegen die Verarbeitung für Zwecke der Direktwerbung erfolgen.',
 };
