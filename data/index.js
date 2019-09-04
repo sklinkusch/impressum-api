9244ae8ccdd4b70c4e25dc1f358fa361addd13ad
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-09-04 10:21:20 +0200

    put source as a new property

--- data/index.js
+++ data/index.js
@@ -3,6 +3,7 @@ const { datenschutz_template } = require('./templates/datenschutz_template');
 exports.impressum = {
   title: impressum_template.map(entry => entry.title),
   content: impressum_template.map(entry => entry.content),
+  source: 'Quelle: https://www.e-recht24.de',
 };
 exports.datenschutz = {
   title: datenschutz_template.map(entry => entry.title),
