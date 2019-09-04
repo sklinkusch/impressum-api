fe7eaa4790617e5afded94b9e988d2c5f72a67c5
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-09-04 09:39:22 +0200

    changed concatenation of tmg

--- data/impressum/tmg.js
+++ data/impressum/tmg.js
@@ -1,7 +1,7 @@
 const { address } = require('../address');
 const streit =
   'Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.';
-const fullArray = address.concat(streit);
+const fullArray = `${address}\n${streit}`;
 exports.tmg = {
   title: 'Angaben gemäß § 5 TMG',
   content: fullArray,
