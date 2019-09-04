f1c6a662b6bda65a09511375c24efa0fbfa4bbb2
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-09-04 09:37:54 +0200

    change address format

--- data/address/index.js
+++ data/address/index.js
@@ -1,7 +1,2 @@
-exports.address = [
-  'Peter R. Stuhlmann',
-  'Martha-Arendsee-Str. 10',
-  '12681 Berlin',
-  'Mobil: +49 1578 5630944',
-  'E-Mail: info@peter-stuhlmann.de',
-];
+exports.address =
+  'Peter R. Stuhlmann\nMartha-Arendsee-Str. 10\n12681 Berlin\nMobil: +49 1578 5630944\nE-Mail: info@peter-stuhlmann.de';
