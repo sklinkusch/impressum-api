fd8e21f369b25111377842541272c72df3627773
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-09-09 09:55:20 +0200

    add endpoint timestamp

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const { impressum, datenschutz } = require('./data');
+const { min_timestamp: timestamp } = require('./timestamp/');
 
+app.get('/timestamp', (req, res) => res.json({ timestamp }));
 app.get('/impressum', (req, res) => res.json(impressum));
 app.get('/datenschutz', (req, res) => res.json(datenschutz));
 
