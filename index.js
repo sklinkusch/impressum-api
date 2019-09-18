020ebc83ad30e090521f9e0eb992b818a6c81d41
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-09-18 09:20:49 +0200

    added header against CORS error

--- index.js
+++ index.js
@@ -4,8 +4,20 @@ const port = 3000;
 const { impressum, datenschutz } = require('./data');
 const { max_timestamp: timestamp } = require('./timestamp/');
 
-app.get('/timestamp', (req, res) => res.json({ timestamp }));
-app.get('/impressum', (req, res) => res.json(impressum));
-app.get('/datenschutz', (req, res) => res.json(datenschutz));
+app.get('/timestamp', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Content-Type', 'application/json');
+  return res.json({ timestamp });
+});
+app.get('/impressum', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Content-Type', 'application/json');
+  return res.json(impressum);
+});
+app.get('/datenschutz', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Content-Type', 'application/json');
+  return res.json(datenschutz);
+});
 
 app.listen(port, () => console.log(`API listening on port ${port}`));
