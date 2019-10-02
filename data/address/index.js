f7cf00b253fe0a252ed47bc9c52b9b7804b7f82b
Stefan Klinkusch <42465490+sklinkusch@users.noreply.github.com>
2019-10-02 20:17:51 +0200

    modify address to anonymous version

--- data/address/index.js
+++ data/address/index.js
@@ -1,2 +1,2 @@
 exports.address =
-  'Peter R. Stuhlmann\nMartha-Arendsee-Str. 10\n12681 Berlin\nMobil: +49 1578 5630944\nE-Mail: info@peter-stuhlmann.de';
+  'Max Mustermann\nMusterstr. 77\n00000 Musterstadt\nMobil: 12345678\nE-Mail: max@mustermann.de';
